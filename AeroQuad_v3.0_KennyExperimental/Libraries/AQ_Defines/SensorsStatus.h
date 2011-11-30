94a1ce768f1f9a9c06fbec9089686bebe190f8e4
Mattias Welponer <mattias@welponer.net>
2011-11-30 20:28:54 +0100

    ...

---
+++ AeroQuad_v3.0_KennyExperimental/Libraries/AQ_Defines/SensorsStatus.h
@@ -0,0 +1,31 @@
+/*
+  AeroQuad v3.0 - May 2011
+  www.AeroQuad.com
+  Copyright (c) 2011 Ted Carancho.  All rights reserved.
+  An Open Source Arduino based multicopter.
+ 
+  This program is free software: you can redistribute it and/or modify 
+  it under the terms of the GNU General Public License as published by 
+  the Free Software Foundation, either version 3 of the License, or 
+  (at your option) any later version. 
+
+  This program is distributed in the hope that it will be useful, 
+  but WITHOUT ANY WARRANTY; without even the implied warranty of 
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+  GNU General Public License for more details. 
+
+  You should have received a copy of the GNU General Public License 
+  along with this program. If not, see <http://www.gnu.org/licenses/>. 
+*/
+
+#ifndef _AQ_SENSORS_STATE_H_
+#define _AQ_SENSORS_STATE_H_
+
+byte sensorsState = 0;
+
+#define GYRO_BIT_STATE  0X01
+#define ACCEL_BIT_STATE 0x02
+#define MAG_BIT_STATE   0x04
+#define BARO_BIT_STATE  0x08
+
+#endif
